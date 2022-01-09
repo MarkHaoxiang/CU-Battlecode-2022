c6d1c18ccecea5f84a9a976d06f806ff1cf186eb
Zhiyi Liu <96021736+thiciu@users.noreply.github.com>
2022-01-09 14:15:15 +0000

    Matrix and Queue Communicator

--- src/misc/Communicator.java
+++ src/misc/Communicator.java
@@ -3,66 +3,75 @@ package sprintbot.battlecode2022.util;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
-
+import sprintbot.RobotPlayer;
 
 public class Communicator {
+    /* Connection Schema
+     * 0 - 15 : Header
+     * 16 - 16 + Archon Number * : Arcon communication
+     * Other : Unit communication
+     * */
 
-    /**
-     *  Queue-based communication.
-     *  Uses index=14~63 in shared memory.
-     *  Each message consists of 2 bits for event type and 6 bits for compressed location.
-     *  Can hold a maximum of 100 messages simultaneously.
-     */
+    /* Header Schema
+     * Map orientation - 2 bits
+     * Archon number - 2 bits
+     * Current strategy? - 3 bits
+     *
+     * */
 
-    private static RobotController controller;
-    private static int MAP_WIDTH;
-    private static int MAP_HEIGHT;
+    /* Archon Schema
+     *
+     * Shared overall macro strategy (eg. attack certain location) 16 bits
+     * Each archon will then have a 16 bit channel for themselves to give out orders
+     *
+     *   Archon order
+     *   1 bit alternating between rounds - still alive?
+     *   3 bits - spawned unit position (archon will check to ensure no two archons spawn at same location)
+     *   8 bits - location
+     *   4 bits - flag
+     *
+     * */
 
-    private static final int COMPRESSED_SIZE = 8; // compress map to 8*8
-    private static final int BITS_PER_LOCATION = 6; // 8*8 = 2^6
-    private static final int BITS_PER_MESSAGE = 8;
+    /* Unit Schema
+     *
+     *  Round Number % 5 = 0: Mining
+     *  Scouting
+     *  Micro
+     *
+     * */
 
-    private static int X_STEP; // the number of x units compressed to 1
-    private static int Y_STEP;
+    protected static RobotController controller;
+    protected static int MAP_WIDTH;
+    protected static int MAP_HEIGHT;
 
-    private static final int p = 100;
-    private static final int offset = 14;
-    private static int tail = 0; // end of queue content (open end, tail not included)
-    private static int length = 0; // length of queue content
+    protected static final int COMPRESSED_SIZE = 8; // compress map to 8*8
+    protected static final int NUM_OF_COMPRESSED_LOCATIONS = 64;
+    protected static int X_STEP; // the number of x units compressed to 1
+    protected static int Y_STEP;
 
-    // can add one more event type
+    protected static final int BITS_PER_INTEGER = 16;
+
     public enum Event {
-        ARCHON, // 0
-        METAL, // 1  Lead or Gold
-        SOLDIER // 2  Soldier or Sage
+        ARCHON, // Opponent Archon
+        METAL, // Metal & no Miner around
+        SOLDIER // Opponent Soldier or Sage
     }
 
-    public Communicator(RobotController controller) {
-        this.controller = controller;
+    public static final Event[] events = {
+            Event.ARCHON,
+            Event.METAL,
+            Event.SOLDIER
+    };
+
+    public static void init(RobotController controller) {
+        Communicator.controller = controller;
         MAP_WIDTH = controller.getMapWidth();
         MAP_HEIGHT = controller.getMapHeight();
         X_STEP = (MAP_WIDTH + MAP_WIDTH % COMPRESSED_SIZE) / COMPRESSED_SIZE;
         Y_STEP = (MAP_HEIGHT + MAP_HEIGHT % COMPRESSED_SIZE) / COMPRESSED_SIZE;
     }
 
-    /**
-     * Compresses a MapLocation to a number between 0~63 (x-major order).
-     * @param loc a valid location on map
-     * @return    compressed location
-     */
-    public static int compressLocation(MapLocation loc) {
-        int x = loc.x;
-        int y = loc.y;
-        return x / X_STEP + (y / Y_STEP) * COMPRESSED_SIZE;
-    }
-
-    /**
-     * Encodes a message into 8 bits.
-     * @param event            type of event
-     * @param compressed_loc   location of the event
-     * @return                 integer representation of the message
-     */
-    private static int encode(Event event, int compressed_loc) {
+    protected static int eventNum(Event event) {
         int event_num;
         switch (event) {
             case ARCHON:
@@ -77,53 +86,56 @@ public class Communicator {
             default: // won't happen
                 throw new IllegalStateException("Unexpected Event: " + event);
         }
-        return (event_num << BITS_PER_LOCATION) + compressed_loc;
+        return event_num;
     }
 
-    /**
-     * Writes a message to shared memory.
-     * @param event          event type
-     * @param compressed_loc compressed location of the event
-     * @throws GameActionException
-     */
-    public static void push(Event event, int compressed_loc) throws GameActionException {
-        int log = encode(event, compressed_loc);
-        if (length == p) { // no more space
-            return;
+    protected static int[] getCacheArray(Event event) {
+        int[] cache_array;
+        switch (event) {
+            case ARCHON:
+                cache_array = Cache.opponent_archon_compressed_locations;
+                break;
+            case METAL:
+                cache_array = Cache.metal_compressed_locations;
+                break;
+            case SOLDIER:
+                cache_array = Cache.opponent_soldier_compressed_locations;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + event);
         }
-        int id = offset + tail / 2;
-        int num = controller.readSharedArray(id);
-        if (length % 2 == 1) {
-            log = (num - num % (1 << BITS_PER_MESSAGE)) + log;
-        }
-        else {
-            log  = (log << BITS_PER_MESSAGE) + (num % (1 << BITS_PER_MESSAGE));
-        }
-        controller.writeSharedArray(id, log);
-        tail = (tail + 1) % p; // wrap around the end
-        length++;
+        return cache_array;
     }
 
-    // TODO: store decoded information in cache instead of returning
-    /**
-     * Pops and returns the first message in shared memory.
-     * @return the first message as an integer
-     * @throws GameActionException
-     */
-    public static int pop() throws GameActionException {
-        if (length == 0) { // pop empty queue
-            return -1;
+
+    // TODO: Please use OOP when transmitting different message types. No static switch statements.
+
+
+    public static void update(Event event, MapLocation location) throws GameActionException {
+        switch (Cache.age % 5) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
         }
-        int head = (tail - length + p) % p;
-        int id = offset + head / 2;
-        int log = controller.readSharedArray(id);
-        if (head % 2 == 1) {
-            log %= (1 << BITS_PER_MESSAGE);
-        }
-        else {
-            log = (log >> BITS_PER_MESSAGE);
-        }
-        length--;
-        return log;
     }
+
+    // TODO: Compression ideas? Discuss.
+    /**
+     * Compresses a MapLocation to a number between 0~63 (x-major order).
+     * @param loc a valid location on map
+     * @return    compressed location
+     */
+    public static int compressLocation(MapLocation loc) {
+        int x = loc.x;
+        int y = loc.y;
+        return x / X_STEP + (y / Y_STEP) * COMPRESSED_SIZE;
+    }
+
+    // TODO: Test out different ways of message storing, bitset? boolean array? directly read? Discuss.
+
+    // TODO: Should we attempt distributed pathfinding? Discuss.
+
+    // TODO: Please store decoded information in cache!
 }
