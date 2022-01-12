ca8d3488030d2dbd8e414cc2a808fff01f1bc7c4
Zhiyi Liu <96021736+thiciu@users.noreply.github.com>
2022-01-12 20:03:25 +0000

    Added Miner LessGatheringMoveStrategy

--- src/sprintbot/battlecode2022/Miner.java
+++ src/sprintbot/battlecode2022/Miner.java
@@ -4,7 +4,6 @@ import battlecode.common.*;
 import sprintbot.RunnableBot;
 import sprintbot.battlecode2022.util.*;
 
-import javax.naming.OperationNotSupportedException;
 import java.util.*;
 
 
@@ -20,6 +19,7 @@ public class Miner extends RunnableBot
 	// Same here
 	private MineStrategy current_mining_strategy;
 	private final DefaultMineStrategy default_mining_strategy = new DefaultMineStrategy();
+	private final LessGatheringMoveStrategy less_gathering_move_strategy = new LessGatheringMoveStrategy();
 	
 	public Miner(RobotController rc) throws GameActionException
 	{
@@ -129,11 +129,11 @@ public class Miner extends RunnableBot
 						// TODO: Fix bug here where location might not be fetched
 						// TODO: Only report locations not already in use and add back functionality
 						// Update reported location to get the desired target [COMMUNICATE]
-					/*
+
 					MatrixCommunicator.read(Communicator.Event.METAL);
 					move_target = Communicator.getClosestFromCompressedLocationArray(Cache.metal_compressed_locations,
 							Cache.controller.getLocation());
-					 */
+
 						// If nothing is available then choose a random location
 						if (move_target == null)
 							move_target = navigator.randomLocation();
@@ -157,11 +157,11 @@ public class Miner extends RunnableBot
 					// TODO: Fix bug here where location might not be fetched
 					// TODO: Only report locations not already in use and add back functionality
 					// Update reported location to get the desired target [COMMUNICATE]
-					/*
+
 					MatrixCommunicator.read(Communicator.Event.METAL);
 					move_target = Communicator.getClosestFromCompressedLocationArray(Cache.metal_compressed_locations,
 							Cache.controller.getLocation());
-					 */
+
 					// If nothing is available then choose a random location
 					if (move_target == null)
 						move_target = navigator.randomLocation();
@@ -169,6 +169,100 @@ public class Miner extends RunnableBot
 					tot_attempts++;
 					if (tot_attempts >= GIVE_UP_THRESHOLD_TURN) // Give up on further attempts
 						return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	class LessGatheringMoveStrategy implements MoveStrategy {
+		private MapLocation move_target = null;
+		boolean is_random = false; // whether move_target is random or not
+		private final int GIVE_UP_THRESHOLD_TURN = 1;
+		/* Number of turns to give up moving if repeatedly stuck
+		   Note that it is necessary because it might get surrounded by robots
+		   It is also probably related to bytecode limit, but we don't account for that now */
+		private static final int HP_THRESHOLD = 10;
+
+		@Override
+		public boolean move() throws GameActionException
+		{
+			// Re-add the metal location to matrix when HP is too low
+			if (move_target != null && Cache.controller.getHealth() < HP_THRESHOLD) {
+				int move_target_compressed_location = Communicator.compressLocation(move_target);
+				MatrixCommunicator.update(Communicator.Event.METAL, move_target);
+			}
+
+			// Move towards the closest lead in vision if found
+			MapLocation[] lead_spots = Cache.controller.senseNearbyLocationsWithLead(RobotType.MINER.visionRadiusSquared);
+			boolean should_mine = false;
+			MapLocation lead_target = null;
+			if (lead_spots.length > 0)
+			{
+				int min_v = Integer.MAX_VALUE;
+				for (MapLocation lead_spot: lead_spots) {
+					int v = Navigator.travelDistance(Cache.controller.getLocation(), lead_spot);
+					for (MapLocation loc : navigator.adjacentLocationWithCenter(lead_spot)) {
+						if (Cache.controller.canSenseLocation(loc)) {
+							RobotInfo robot = Cache.controller.senseRobotAtLocation(loc);
+							if (robot != null
+									&& robot.getTeam().isPlayer()
+									&& robot.getType() == RobotType.MINER) {
+								v = Integer.MAX_VALUE;
+							}
+						}
+					}
+					if (v < min_v) {
+						min_v = v;
+						lead_target = lead_spot;
+					}
+				}
+				if (min_v < Integer.MAX_VALUE) {
+					should_mine = true;
+				}
+			}
+
+			if (should_mine) {
+				// not report the lead location, but save the location in this miner's instance state
+				move_target = lead_target;
+				is_random = false;
+
+				Navigator.MoveResult move_result = navigator.move(lead_target);
+				if (move_result == Navigator.MoveResult.IMPOSSIBLE ||
+						move_result == Navigator.MoveResult.REACHED)
+					return false;
+			}
+
+			else // If no leads nearby or shouldn't mine nearby lead, either moves towards the closest reported location and erases that location from matrix or chooses a random location
+			{
+				Navigator.MoveResult move_result = navigator.move(move_target);
+				// Only changes the target if moving fails
+				int tot_attempts = 0;
+
+				while (move_result == Navigator.MoveResult.IMPOSSIBLE ||
+						move_result == Navigator.MoveResult.REACHED)
+				{
+					tot_attempts++;
+					if (tot_attempts > GIVE_UP_THRESHOLD_TURN) // Give up on further attempts
+						return false;
+
+					MatrixCommunicator.read(Communicator.Event.METAL);
+					move_target = Communicator.getClosestFromCompressedLocationArray(Cache.metal_compressed_locations,
+							Cache.controller.getLocation());
+					is_random = false;
+
+					// If nothing is available then choose a random location
+					if (move_target == null) {
+						move_target = navigator.randomLocation();
+						is_random = true;
+					}
+
+					move_result = navigator.move(move_target);
+				}
+
+				if (!is_random) {
+					// erase the location from the matrix
+					MatrixCommunicator.update(Communicator.Event.METAL, move_target, false);
 				}
 			}
 			return true;
@@ -191,8 +285,9 @@ public class Miner extends RunnableBot
 					// You can mine multiple times per turn!
 					while (Cache.controller.canMineGold(mineLocation))
 						Cache.controller.mineGold(mineLocation);
-					while (Cache.controller.canMineLead(mineLocation) && Cache.controller.senseLead(mineLocation) > 2)
+					while (Cache.controller.canMineLead(mineLocation) && Cache.controller.senseLead(mineLocation) > 2) {
 						Cache.controller.mineLead(mineLocation);
+					}
 				}
 			return true;
 		}
@@ -227,7 +322,8 @@ public class Miner extends RunnableBot
 		current_mining_strategy = default_mining_strategy;
 		current_mining_strategy.mine();
 		
-		current_moving_strategy = default_moving_strategy;
+//		current_moving_strategy = default_moving_strategy;
+		current_moving_strategy = less_gathering_move_strategy;
 		current_moving_strategy.move();
 	}
 }
